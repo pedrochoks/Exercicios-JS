063ed9bd786ebfc107263d48e9cd0b6c05324313
pedrochoks <pedro.a.gomes12@aluno.senai.br>
2025-09-08 08:55:10 -0300

    08/09

---
+++ EX Repeticao.js
@@ -0,0 +1,81 @@
+//! Exercicios basico
+
+//? Exercicio 1
+// for(let i = 1; i <=10; i++)
+// {
+//     console.log(i)
+// }
+
+//?Exercicio 2
+// let numero = parseFloat(prompt("Digite um numero"))
+
+// for(let i = 1; i <11; i++)
+// {
+//     console.log(i*numero);
+// }
+
+//?Exercicio 3
+// let numero = parseInt(prompt("Digite um numero"))
+// let i = 1
+// let soma = 0;
+
+// while(i <= numero)
+// {
+//     soma += i
+//     i++;
+// }
+// console.log(soma);
+
+
+//! Nivel Intermediario
+//? Exercicio 1
+
+// for(i = 1; i <51; i++)
+// {
+//     if(i % 2 == 0)
+//     {
+//         console.log(i);
+//     }
+// }
+
+//? Exercicio 2
+// let numeroSecreto = Math.floor(Math.random() * 100) + 1; //* Math random gera um numero entre 0 e 1, math floor arredonda ele para baixo
+// let palpite = 0;
+
+// while (palpite !== numeroSecreto) {
+//     palpite = prompt("Digite o numero e tente acertar");
+//     if (palpite < numeroSecreto) {
+//         alert("O numero secreto e maior")
+//     }
+//     else if (palpite > numeroSecreto) {
+//         alert("O numero secreto e menor")
+//     }
+//     else {
+//         alert("Parabens vc acertou o numero era " + numeroSecreto)
+//         palpite = numeroSecreto;
+//     }
+// }
+
+//? Exercicio 3
+// let numero = parseInt(prompt("Digite um numero"))
+
+// while(numero !== -0)
+// {
+//     numero --;
+//     console.log(numero);
+// }
+
+//! NIVEL AVANCADO
+//?Exercicio 1
+
+//?Exercicio 2
+// let numero = parseInt(prompt("Digite um numero"));
+// let fatoracao = 1;
+
+// for( let i = 1; i <= numero; i++)
+// {
+//     fatoracao *= i;
+// }
+// console.log(fatoracao);
+
+//?Exercicio 3  
